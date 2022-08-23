df9467fbbc72fa517895613b0b77f8d313fd5051
Aaron Chris <rooneychris83@gmail.com>
2022-08-23 21:19:05 +0100

    writefile added

--- core_modules/fs_module.js
+++ core_modules/fs_module.js
@@ -1,7 +1,19 @@
-const fs = require('fs')
-const path = require('path')
+const fs = require("fs");
+const path = require("path");
 
-// create a file 
-fs.mkdir(path.join(__dirname, 'fs'), [], err => {
+// create a file
+// fs.mkdir(path.join(__dirname, "fs"), [], (err) => {
+//   if (err) throw err;
+//   console.log("folder created successfully..");
+// });
 
-})
+// console.log('async')
+
+fs.writeFile(
+  path.join(__dirname, "fs", "index.js"),
+  "console.log(`fuck you`)",
+  (err) => {
+    if (err) throw err;
+    console.log("Nice file made");
+  }
+);
